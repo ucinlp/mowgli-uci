6cca2165116be6db15b24dc39f5a4d0f22636ba9
rloganiv <rloganiv@gmail.com>
2020-02-28 12:33:30 -0800

    Basic project setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="graphify",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "allennlp>=0.9.0",
+        "numpy>=1.17.4",
+        "spacy>=2.2.3",
+        "tqdm>=4.40.2",
+        "wordfreq>=2.2.1"
+    ]
+)
