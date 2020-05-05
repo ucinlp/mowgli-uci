ea4df42c97aab22f82ce1122de4c9309562d666e
rloganiv <rloganiv@gmail.com>
2020-05-05 11:52:41 -0700

    Added fasttext dependency

--- setup.py
+++ setup.py
@@ -12,8 +12,10 @@ setup(
         "numpy>=1.17.4",
         "spacy>=2.2.3",
         "tqdm>=4.40.2",
-        "wordfreq>=2.2.1"
+        "wordfreq>=2.2.1",
+        "fasttext",
     ],
+    dependency_links=['git+https://github.com/facebookresearch/fastText.git/@v0.9.2#egg=fasttext-0'],
     tests_require=test_deps,
     extras_require={
         'test': test_deps
